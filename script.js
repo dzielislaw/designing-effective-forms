bcf8f16d58501381cc6710ff69499d6901df67c8
Aleksander Roman <aleksander.roman@student.pk.edu.pl>
2025-05-13 17:08:09 +0200

    refactoring

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let clickCount = 0;
 
 const countryInput = document.getElementById('country');
+const countryCodeInput = document.getElementById('countryCode');
 const myForm = document.getElementById('form');
 const modal = document.getElementById('form-feedback-modal');
 const clicksInfo = document.getElementById('click-count');
@@ -19,6 +20,11 @@ async function fetchAndFillCountries() {
         const data = await response.json();
         const countries = data.map(country => country.name.common);
         countryInput.innerHTML = countries.map(country => `<option value="${country}">${country}</option>`).join('');
+
+        $('.select2').select2({
+            placeholder: "Wybierz kraj",
+            allowClear: true
+        });
     } catch (error) {
         console.error('Wystąpił błąd:', error);
     }
@@ -29,7 +35,11 @@ function getCountryByIP() {
         .then(response => response.json())
         .then(data => {
             const country = data.country;
+            console.log(country);
             // TODO inject country to form and call getCountryCode(country) function
+            countryInput.value = country;
+            $('.select2').val(country).trigger('change');
+            getCountryCode(country)
         })
         .catch(error => {
             console.error('Błąd pobierania danych z serwera GeoJS:', error);
@@ -49,16 +59,33 @@ function getCountryCode(countryName) {
     .then(data => {        
         const countryCode = data[0].idd.root + data[0].idd.suffixes.join("")
         // TODO inject countryCode to form
+        console.log(countryCode);
+        countryCodeInput.value = countryCode;
     })
     .catch(error => {
         console.error('Wystąpił błąd:', error);
     });
 }
 
+$('#country').on('change', function() {
+    getCountryCode($(this).val());
+});
+
+function toggleVatInput() {
+        const checkbox = document.getElementById('vatUE');
+        const vatInputContainer = document.getElementById('vatInputContainer');
+        
+        if (checkbox.checked) {
+            vatInputContainer.style.display = 'block';
+        } else {
+            vatInputContainer.style.display = 'none';
+        }
+    }
 
 (() => {
     // nasłuchiwania na zdarzenie kliknięcia myszką
     document.addEventListener('click', handleClick);
 
     fetchAndFillCountries();
+    getCountryByIP();
 })()
